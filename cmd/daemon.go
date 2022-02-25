9ac6749d8fdf578ead22f377d1fc29fa93f682cd
lilo <luolee.me@gmail.com>
2022-02-25 12:11:32 +0800

    add daemon sub command fo *nix platform

---
+++ cmd/daemon.go
@@ -0,0 +1,65 @@
+/*
+Copyright © 2022 liluo <luolee.me@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/sevlyar/go-daemon"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var daemonCmd = &cobra.Command{
+	Use:   "daemon",
+	Short: "run gvn daemon",
+	Long:  `Run gvn as a deamon service, all the args must be consistent with up sub-command`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		pidFile := filepath.Join(os.TempDir(), "gvn.pid")
+		logFile := filepath.Join(os.TempDir(), "gvn.log")
+		pwd, err := os.Getwd()
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"ERROR": err,
+			}).Fatal("Current dir doesn't accessable")
+		}
+		cntxt := &daemon.Context{
+			PidFileName: pidFile,
+			PidFilePerm: 0644,
+			LogFileName: logFile,
+			LogFilePerm: 0640,
+			WorkDir:     pwd,
+			Umask:       027,
+			Args:        args,
+		}
+		d, err := cntxt.Reborn()
+		if err != nil {
+			logrus.Fatal("Unable to run gvn as daemon" + err.Error())
+		}
+		if d != nil {
+			return
+		}
+		defer cntxt.Release()
+		upCommand(cmd)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(daemonCmd)
+	daemonCmd.Flags().Uint32P("pid", "p", 0, "the gvn process pid to be stop")
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/ipfs/go-datastore v0.5.0
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
 	github.com/libp2p/go-libp2p v0.17.0
 	github.com/libp2p/go-libp2p-core v0.13.0
 	github.com/libp2p/go-libp2p-discovery v0.6.0
@@ -12,6 +13,7 @@ require (
 	github.com/libp2p/go-libp2p-pubsub v0.6.1
 	github.com/liloew/wireguard-go v0.0.0-20220224014633-9cd745e6f114
 	github.com/multiformats/go-multiaddr v0.4.0
+	github.com/sevlyar/go-daemon v0.1.5
 	github.com/sirupsen/logrus v1.6.0
 	github.com/songgao/packets v0.0.0-20160404182456-549a10cd4091
 	github.com/songgao/water v0.0.0-20200317203138-2b4b6d7c09d8
