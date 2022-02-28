3c0c7d4d2cf7c665e1b0c6369db61fd56c62b127
lilo <luolee.me@gmail.com>
2022-02-28 09:51:40 +0800

    fix typo

--- cmd/init.go
+++ cmd/init.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2022 liluo <luolee.me@gmail.com>
+Copyright © 2022 lilo <luolee.me@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
